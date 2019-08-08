6675f3970eb2c983794bf5b5aa6f0a2dbb7579b3
Lukas Bock <lukas.bock@uni-oldenburg.de>
2019-08-09 00:23:12 +0200

    lodash.template fixed

--- misc/changeadditionalpage.php
+++ misc/changeadditionalpage.php
@@ -8,7 +8,7 @@ $showInFooter = $_POST['showInFooter'];
 if ($_POST['action'] == "Edit") {
     include '../database/SQLAdditionalPagesActions.php';
     $pagesActions = new SQLAdditionalPagesActions();
-    ($pagesActions->editPageEntry($title, $content, $showInFooter, $id)) ? header('Location: ../core/additionalpages.php') : header('Location: ../core/error.php?reason=dberror');
+    ($pagesActions->editPageEntry($title, $content, $showInFooter, $id)) ? header('Location: ../core/additionalPages.php') : header('Location: ../core/error.php?reason=dberror');
 } elseif ($_POST['action'] == "Delete") {
     include '../database/SQLAdditionalPagesActions.php';
     $pagesActions = new SQLAdditionalPagesActions();
@@ -16,7 +16,7 @@ if ($_POST['action'] == "Edit") {
 } elseif ($_POST['action'] == "New") {
     include '../database/SQLAdditionalPagesActions.php';
     $pagesActions = new SQLAdditionalPagesActions();
-    ($pagesActions->newPageEntry($title, $content, $showInFooter)) ? header('Location: ../core/additionalpages.php') : header('Location: ../core/error.php?reason=dberror');
+    ($pagesActions->newPageEntry($title, $content, $showInFooter)) ? header('Location: ../core/additionalPages.php') : header('Location: ../core/error.php?reason=dberror');
 } else {
     header('Location: ../core/error.php?reason=actionnotfound');
     die();
