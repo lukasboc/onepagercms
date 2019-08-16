ad234fbbdf1021f8f594b8bb671337053057474e
Lukas Bock <lukas.bock@uni-oldenburg.de>
2019-08-17 01:07:59 +0200

    added logo to backend

--- core/inc/header.php
+++ core/inc/header.php
@@ -7,7 +7,9 @@ if (!isset($_SESSION['profile'])) {
 }
 echo '
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand btn disabled" href="#">OnePagerCMS</a>
+    <a class="navbar-brand" href="#">
+    <img src="../img/logo/opcms_dark.png" height="35" alt="OnePager CMS">
+</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
