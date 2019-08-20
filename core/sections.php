ca912f2a3a13b3a4f549b3807e3a33d42e28fb22
Lukas Bock <lukas.bock@uni-oldenburg.de>
2019-08-21 01:12:17 +0200

    fixed new sections dropdown

--- core/sections.php
+++ core/sections.php
@@ -104,7 +104,7 @@ for($i=0; sizeof($sections) > $i; $i++){
     <form action="../misc/newsection.php" method="post">
         <div class="form-group">
     <label for="types">Type:</label>
-    <select class="form-control col-1" name="type" id="types">
+            <select class="form-control col-sm-3" name="type" id="types">
         <option>standard</option>
         <option>icons</option>
         <option>contact</option>
