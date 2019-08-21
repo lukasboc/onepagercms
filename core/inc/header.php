1e9ac58614105457144e06b598b67798aabfd2b5
Simon Schneider <Schneider.Simon1@gmx.de>
2019-08-21 22:08:51 +0200

    Logo eingefügt Sir!

--- core/inc/header.php
+++ core/inc/header.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['profile'])) {
 echo '
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">
-    <img src="../img/logo/opcms_dark.png" height="35" alt="OnePager CMS">
+    <img src="../img/logo/logo_black.png" height="35" alt="OnePager CMS">
 </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
