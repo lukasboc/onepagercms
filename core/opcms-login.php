d7de24962634477e85604434c957704d40c9de63
Lukas Bock <lukas.bock@uni-oldenburg.de>
2019-08-22 21:48:02 +0200

    added logo again, added else-cases to changelogo and changewebsitetitle

--- core/opcms-login.php
+++ core/opcms-login.php
@@ -9,7 +9,7 @@ $settingactions = new SQLSettingActions();
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="../css/login.css" rel="stylesheet">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 </head>
 <body>
 
