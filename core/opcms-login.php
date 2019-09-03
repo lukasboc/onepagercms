9f52148c90de85f8e342feabe10e8bf3d70e5fe8
Lukas Bock <lukas.bock@uni-oldenburg.de>
2019-09-03 19:17:46 +0200

    added favicon to login and resetpass

--- core/opcms-login.php
+++ core/opcms-login.php
@@ -10,6 +10,7 @@ $settingactions = new SQLSettingActions();
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="../css/login.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <?php if (file_exists('../img/favicon/opcms48x48.ico')) echo "<link rel=\"shortcut icon\" href=\"../img/favicon/opcms48x48.ico\">"; ?>
 </head>
 <body>
 
