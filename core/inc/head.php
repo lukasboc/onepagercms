d2fec267cf9bc5d6589efa2a65dbe412bb7fa8de
lukas <lukas.bock@lubomedia.de>
2026-05-25 17:09:47 +0200

    rm pages/vendor

--- core/inc/head.php
+++ core/inc/head.php
@@ -5,7 +5,7 @@ echo '
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="../plugins/vendor/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
     <link rel="stylesheet" href="../css/customCSS.css">
     <link rel="stylesheet" href="../plugins/Trumbowyg/dist/ui/trumbowyg.min.css">';
 if (file_exists('../img/favicon/opcms48x48.ico')) echo "<link rel=\"shortcut icon\" href=\"../img/favicon/opcms48x48.ico\">";
