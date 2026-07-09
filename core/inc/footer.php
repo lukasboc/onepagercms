9ad93ef486d566d621e377e39207aba20e22e940
lukas <lukas.bock@lubomedia.de>
2026-07-09 20:28:13 +0200

    fix col

--- core/inc/footer.php
+++ core/inc/footer.php
@@ -6,7 +6,7 @@ echo '
         </main>
         <footer class="footer footer-center text-sm text-base-content/60 py-3 border-t border-base-300 bg-base-100">
             <div>
-                <a class="link" href="https://www.buymeacoffee.com/lukasboc" target="_blank">Support OPCMS</a> | Version ' . (defined('OPCMS_VERSION') ? OPCMS_VERSION : '1.2.0') . ' | Thanks for using <a class="link" href="https://onepagercms.de">OPCMS</a>!
+                <p><a class="link" href="https://www.buymeacoffee.com/lukasboc" target="_blank">Support OPCMS</a> | Version ' . (defined('OPCMS_VERSION') ? OPCMS_VERSION : '1.2.0') . ' | Thanks for using <a class="link" href="https://onepagercms.de">OPCMS</a>!</p>
             </div>
         </footer>
     </div>
